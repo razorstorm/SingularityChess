533557893ff01cd676f7899bcf43254689e19886
razorstorm <petrifiednightmares@gmail.com>
2012-12-22 14:04:30 -0800

    ACRA is set up

---
+++ src/com/petrifiednightmares/singularityChess/logging/ACRA_Reporting.java
@@ -0,0 +1,6 @@
+package com.petrifiednightmares.singularityChess.logging;
+
+public class ACRA_Reporting
+{
+	public final static String	FORM_KEY	= "dC15X1laWUVYR1FQOGdKd1h6SGN3dHc6MQ";
+}
--- src/com/petrifiednightmares/singularityChess/logic/Game.java
+++ src/com/petrifiednightmares/singularityChess/logic/Game.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 
+import org.acra.ACRA;
+
 import android.graphics.Canvas;
 
 import com.petrifiednightmares.singularityChess.GameDrawingPanel;
@@ -119,6 +121,7 @@ public class Game extends GameDrawable
 			initialize(board, gui, _gameType);
 			gdp.displayMessage("Resume failed, starting new game against human");
 
+			ACRA.getErrorReporter().handleSilentException(e);
 			e.printStackTrace();
 		}
 		finally
